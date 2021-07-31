22eabc594e1954df9cb0131c529c897d9b847910
Siddhant100000 <siddhant.jk@gmail.com>
2021-07-31 15:51:05 +0530

    Basic burger builder app completed

--- src/components/Burger/OrderSummary/orderSummary.js
+++ src/components/Burger/OrderSummary/orderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Auxiliary from '../../../hoc/Auxiliary'
 import Button from '../../Ui/Button/Button';
 
-export default function OrderSummary(props) {
-    
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
+class OrderSummary extends Component {
+
+    componentDidUpdate(){
+        console.log("[OrderSummary.js] Update Cycle commenced ");
+    }
+
+    render (){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                        <li key = {igKey}>
+                            <span style = {{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+                        </li>
+                        );
+            });
         return (
-                <li key = {igKey}>
-                    <span style = {{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-                </li>
-                );
-    });
 
-    return (
-
-       <Auxiliary>
-           <h3>Your Order</h3>
-           <p>A delicious burger with the following ingredients</p>
-           <ul>
-                {ingredientSummary}
-           </ul>
-           <p><strong>Total price : {props.price.toFixed(2)}</strong></p>
-           <p>Continue to check out !</p>
-           <Button btnType = "Success" clicked = {props.purchaseCotinue} >Continue</Button>
-           <Button btnType = "Danger" clicked = {props.purchaseCanceled}>Cancel</Button>
-       </Auxiliary>
-    );
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                        {ingredientSummary}
+                </ul>
+                <p><strong>Total price : {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to check out !</p>
+                <Button btnType = "Success" clicked = {this.props.purchaseCotinue} >Continue</Button>
+                <Button btnType = "Danger" clicked = {this.props.purchaseCanceled}>Cancel</Button>
+            </Auxiliary>
+        );
+    }
 };
+
+
+export default OrderSummary
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import classes from './Modal.module.css'
 import Auxiliary from '../../hoc/Auxiliary'
 import BackDrop from '../Ui/BackDrop/BackDrop'
 
-export default function Modal(props) {
-    return (
+class Modal extends Component {
+    
+        shouldComponentUpdate(nextProps, nextState){
+           return nextProps.show !== this.props.show;
+        }
+
+        componentDidUpdate(){
+            console.log("[Modal.js] Will Update!")
+        }
+
+    
+    render(){
+        return (
         <Auxiliary>
-            <BackDrop show = {props.show}/>
+            <BackDrop show = {this.props.show}/>
             <div className={classes.Modal}
             style ={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)' ,
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)' ,
+                opacity: this.props.show ? '1' : '0'
             }}>
-            {props.children}
+            {this.props.children}
             </div>
         </Auxiliary>
         
-    )
-}
+        );
+    };
+};
+
+export default Modal;
